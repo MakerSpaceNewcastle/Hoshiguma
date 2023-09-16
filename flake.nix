916e4f0fd74b661e3f4b434743bf44e044c9fa41
Dan Nixon <dan@dan-nixon.com>
2023-09-16 14:40:38 +0100

    Use a single flake for the entire repository

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  inputs = {
+    ravedude.url = "github:Rahix/avr-hal?dir=ravedude";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ravedude }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        ravedude' = ravedude.packages."${system}".default;
+      in rec {
+        devShell = pkgs.mkShell {
+          packages = with pkgs; [
+            # Common
+            rustup
+            pkg-config
+
+            # koishi firmware
+            avrdude
+            ravedude'
+            pkgs.pkgsCross.avr.buildPackages.gcc
+
+            # koishi firmware in-simulator tests
+            clang
+            libelf
+            simavr
+            zlib
+
+            # koishi telemetry receiver demo
+            systemd
+          ];
+
+          LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+        };
+      }
+    );
+}
