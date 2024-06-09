a5b7e8746d53cd6ee609b2902f70a90ad1850451
Dan Nixon <dan@dan-nixon.com>
2024-06-09 13:21:02 +0100

    wip

--- satori/firmware/src/hal/pins.rs
+++ satori/firmware/src/hal/pins.rs
@@ -2,28 +2,28 @@ use atmega_hal::port::Pin;
 
 avr_hal_generic::renamed_pins! {
     pub struct Pins {
-        pub relay1: atmega_hal::port::PC3 = pc3, /// A3
-        pub relay2: atmega_hal::port::PC2 = pc2, /// A2
-        pub relay3: atmega_hal::port::PC1 = pc1, /// A1
-        pub relay4: atmega_hal::port::PC0 = pc0, /// A0
-        pub relay5: atmega_hal::port::PB4 = pb4, /// D12
-        pub relay6: atmega_hal::port::PB3 = pb3, /// D11
-        pub relay7: atmega_hal::port::PB2 = pb2, /// D10
-        pub relay8: atmega_hal::port::PB1 = pb1, /// D9
+        pub led: atmega_hal::port::PB5 = pb5,
 
-        pub in1: atmega_hal::port::PD2 = pd2, /// D2
-        pub in2: atmega_hal::port::PD3 = pd3, /// D3
-        pub in3: atmega_hal::port::PD4 = pd4, /// D4
-        pub in4: atmega_hal::port::PD5 = pd5, /// D5
-        pub in5: atmega_hal::port::PD6 = pd6, /// D6
-        pub in6: atmega_hal::port::PD7 = pd7, /// D7
-
-        pub a4: atmega_hal::port::PC4 = pc4,
-        pub a5: atmega_hal::port::PC5 = pc5,
         pub d0: atmega_hal::port::PD0 = pd0,
         pub d1: atmega_hal::port::PD1 = pd1,
+        pub d2: atmega_hal::port::PD2 = pd2,
+        pub d3: atmega_hal::port::PD3 = pd3,
+        pub d4: atmega_hal::port::PD4 = pd4,
+        pub d5: atmega_hal::port::PD5 = pd5,
+        pub d6: atmega_hal::port::PD6 = pd6,
+        pub d7: atmega_hal::port::PD7 = pd7,
         pub d8: atmega_hal::port::PB0 = pb0,
-        pub d13: atmega_hal::port::PB5 = pb5,
+        pub d9: atmega_hal::port::PB1 = pb1,
+        pub d10: atmega_hal::port::PB2 = pb2,
+        pub d11: atmega_hal::port::PB3 = pb3,
+        pub d12: atmega_hal::port::PB4 = pb4,
+
+        pub a0: atmega_hal::port::PC0 = pc0,
+        pub a1: atmega_hal::port::PC1 = pc1,
+        pub a2: atmega_hal::port::PC2 = pc2,
+        pub a3: atmega_hal::port::PC3 = pc3,
+        pub a4: atmega_hal::port::PC4 = pc4,
+        pub a5: atmega_hal::port::PC5 = pc5,
     }
 
     impl Pins {
--- satori/firmware/src/main.rs
+++ satori/firmware/src/main.rs
@@ -15,7 +15,7 @@ fn panic(_info: &core::panic::PanicInfo) -> ! {
     let dp = unsafe { atmega_hal::Peripherals::steal() };
     let pins = hal::Pins::with_mcu_pins(atmega_hal::pins!(dp));
 
-    let mut led = pins.d13.into_output();
+    let mut led = pins.led.into_output();
     loop {
         led.toggle();
         hal::Delay::new().delay_ms(50u16);
@@ -31,15 +31,67 @@ fn main() -> ! {
     unsafe { avr_device::interrupt::enable() };
 
     let mut serial = serial!(dp, pins, 57600);
-    serial.write_str("feck arse drink").unwrap();
+    serial.write_str("feck arse drink\n").unwrap();
 
-    let mut led = pins.d13.into_output();
+    let _d7 = pins.d7.into_pull_up_input();
+    let _d9 = pins.d9.into_pull_up_input();
+
+    // Enable the PCINT0 and PCINT2 interrupts
+    // See datasheet: 12.2.4 PCICR - Pin Change Interrupt Control Register
+    dp.EXINT.pcicr.write(|w| unsafe { w.bits(0b00000101) });
+
+    // Enable pin change interrupts on PCINT23 which is pin PD7 (= d7)
+    // See datasheet: 13.3.3 Alternate Functions of Port D
+    dp.EXINT.pcmsk2.write(|w| w.bits(0b10000000));
+
+    // Enable pin change interrupts on PCINT1 which is pin PB1 (= d9)
+    // See datasheet: 13.3.1 Alternate Functions of Port B
+    dp.EXINT.pcmsk0.write(|w| w.bits(0b00000010));
+
+    unsafe { avr_device::interrupt::enable() };
+
+    let mut led = pins.led.into_output();
 
     loop {
         let _time = crate::hal::millis();
+
+        let count_0 = avr_device::interrupt::free(|_cs| {
+            let count = COUNT_0.load(Ordering::SeqCst);
+            COUNT_0.store(0, Ordering::SeqCst);
+            count
+        });
+        ufmt::uwrite!(serial, "count 0 = {}\n", count_0).unwrap();
+
+        let count_2 = avr_device::interrupt::free(|_cs| {
+            let count = COUNT_2.load(Ordering::SeqCst);
+            COUNT_2.store(0, Ordering::SeqCst);
+            count
+        });
+        ufmt::uwrite!(serial, "count 2 = {}\n", count_2).unwrap();
 
         // TODO
         led.toggle();
         hal::Delay::new().delay_ms(500u16);
     }
+}
+
+use core::sync::atomic::{AtomicU8, Ordering};
+
+static COUNT_0: AtomicU8 = AtomicU8::new(0);
+static COUNT_2: AtomicU8 = AtomicU8::new(0);
+
+#[avr_device::interrupt(atmega328p)]
+#[allow(non_snake_case)]
+fn PCINT0() {
+    let mut count = COUNT_0.load(Ordering::SeqCst);
+    count += 1;
+    COUNT_0.store(count, Ordering::SeqCst);
+}
+
+#[avr_device::interrupt(atmega328p)]
+#[allow(non_snake_case)]
+fn PCINT2() {
+    let mut count = COUNT_2.load(Ordering::SeqCst);
+    count += 1;
+    COUNT_2.store(count, Ordering::SeqCst);
 }
