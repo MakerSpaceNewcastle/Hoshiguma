7ba423b6adae5993b90586fe51b11f57eba5c9e8
Dan Nixon <dan@dan-nixon.com>
2024-06-09 22:21:26 +0100

    wip

--- satori/firmware/src/status.rs
+++ satori/firmware/src/status.rs
@@ -1,3 +1,72 @@
+use serde::Deserialize;
+use enumset::{EnumSetType, EnumSet};
+
+type TimeMillis = u32;
+
+#[derive(Debug, Deserialize)]
+#[allow(dead_code)]
+pub struct Message {
+    time: TimeMillis,
+    iteration_id: Option<u32>,
+    pub payload: Payload,
+}
+
+#[derive(Debug, Deserialize)]
+pub enum Payload {
+    Boot(Boot),
+    Panic(Panic),
+    StatusChanged(Status),
+}
+
+#[derive(Debug, Deserialize)]
+#[allow(dead_code)]
+pub struct Boot {
+    name: String,
+    pub git_revision: String,
+}
+
+#[derive(Debug, Deserialize)]
+#[allow(dead_code)]
+pub struct Panic {
+    file: Option<String>,
+    line: Option<u32>,
+    column: Option<u32>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Status {
+    temperature: Temperatures,
+    coolant_level: CoolantLevel,
+    coolant_pump_rpm: f32,
+    coolant_flow_rate: f32,
+
+    potential_problems: Vec<()>,
+    problems: Vec<()>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Temperatures {
+    coolant_flow: f32,
+    coolant_return: f32,
+
+    coolant_resevoir_upper: f32,
+    coolant_resevoir_lower: f32,
+
+    coolant_pump: f32,
+
+    room_ambient: f32,
+    laser_bay: f32,
+    electronics_bay: f32,
+}
+
+#[derive(Debug, Deserialize)]
+enum CoolantLevel {
+    Full,
+    Low,
+    CriticallyLow,
+}
+
+#[derive(Debug, Deserialize)]
 enum MachineProblem {
     CoolentLevelInsufficient,
     CoolantFlowRateInsufficient,
