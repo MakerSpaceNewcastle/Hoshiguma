dade987ee21c9eb4db98205b19550555740a4855
Dan Nixon <dan@dan-nixon.com>
2024-06-12 18:08:39 +0100

    wip

---
+++ satori/firmware/src/reporting.rs
@@ -0,0 +1,45 @@
+pub(super) fn report<USART: atmega_hal::usart::UsartOps<atmega_hal::Atmega, TX, RX>, TX, RX>(
+    serial: &mut Usart<USART, TX, RX>,
+    msg: &Message,
+) {
+    let data = postcard::to_vec_cobs::<Message, 128>(&msg).unwrap();
+
+    for i in data {
+        serial.write_byte(i);
+    }
+    serial.flush();
+}
+
+pub(crate) fn boot<USART: atmega_hal::usart::UsartOps<atmega_hal::Atmega, TX, RX>, TX, RX>(
+    serial: &mut Usart<USART, TX, RX>,
+) {
+    report(
+        serial,
+        &Message::new(None, Payload::Boot(BootPayload::default())),
+    );
+}
+
+pub(crate) fn panic<USART: atmega_hal::usart::UsartOps<atmega_hal::Atmega, TX, RX>, TX, RX>(
+    serial: &mut Usart<USART, TX, RX>,
+    info: &core::panic::PanicInfo,
+) {
+    report(
+        serial,
+        &Message::new(None, Payload::Panic(PanicPayload::from(info))),
+    );
+}
+
+pub(crate) fn status<USART: atmega_hal::usart::UsartOps<atmega_hal::Atmega, TX, RX>, TX, RX, T>(
+    serial: &mut Usart<USART, TX, RX>,
+    iteration_id: u32,
+    status_payload: T,
+) where
+    Payload: From<T>,
+{
+    let msg = Message::new(Some(iteration_id), status_payload.into());
+    report(serial, &msg);
+}
+
+
+
+
--- telemetry/protocols/src/satori.rs
+++ telemetry/protocols/src/satori.rs
@@ -1,6 +1,50 @@
 use serde::{Deserialize, Serialize};
+use enumset::{EnumSet, EnumSetType};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub enum Payload {
-    StateChanged(()),
+    StateChanged(Status),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Status {
+    temperature: Temperatures,
+    coolant_level: CoolantLevel,
+    coolant_pump_rpm: f32,
+    coolant_flow_rate: f32,
+
+    potential_problems: EnumSet<MachineProblem>,
+    problems: EnumSet<MachineProblem>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Temperatures {
+    pub coolant_flow: f32,
+    pub coolant_return: f32,
+
+    pub coolant_resevoir_upper: f32,
+    pub coolant_resevoir_lower: f32,
+
+    pub coolant_pump: f32,
+
+    pub room_ambient: f32,
+    pub laser_bay: f32,
+    pub electronics_bay: f32,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum CoolantLevel {
+    Full,
+    Low,
+    CriticallyLow,
+}
+
+#[derive(Debug, Serialize, Deserialize, EnumSetType)]
+pub enum MachineProblem {
+    CoolentLevelInsufficient,
+    CoolantFlowRateInsufficient,
+    CoolantFlowOvertemperature,
+    CoolantReturnOvertemperature,
+    ElectronicsBayOvertemperature,
+    LaserBayOvertemperature,
 }
