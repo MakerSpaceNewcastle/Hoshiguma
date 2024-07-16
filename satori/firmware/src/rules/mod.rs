acb79c4113382023ba2d6111a1a029abe8c96781
Dan Nixon <dan@dan-nixon.com>
2024-07-16 19:20:29 +0100

    wip

--- satori/firmware/src/rules/mod.rs
+++ satori/firmware/src/rules/mod.rs
@@ -1,7 +1,7 @@
 use hoshiguma_foundational_data::{
     satori::{
-        CoolantLevel, MachineProblem, MachineProblems, ObservedState, PotentialMachineProblems,
-        ProblemKind, ProblemSeverity,
+        CoolantLevel, MachineProblem, MachineProblems, ObservedState, PotentialMachineProblem,
+        PotentialMachineProblems, ProblemKind, ProblemSeverity,
     },
     TimeMillis,
 };
@@ -30,19 +30,56 @@ pub(super) fn evaluate(mut ctx: RuleEvaluationContext) {
 ///     - empty: immediate problem
 ///     - unknown: potential problem, promote to problem in 5 minutes
 fn coolant_level(ctx: &mut RuleEvaluationContext) {
-    let active_level_potential_problem = ctx
+    let sensor_potential_problem = ctx
+        .potential_problems
+        .iter()
+        .find(|i| i.problem.kind == ProblemKind::CoolantLevelSensorFault);
+
+    let level_potential_problem = ctx
         .potential_problems
         .iter()
         .find(|i| i.problem.kind == ProblemKind::CoolantLevelInsufficient);
 
-    // TODO
     match &ctx.state.coolant_level {
         Some(level) => match level {
             CoolantLevel::Full => {
                 // Do nothing.
                 // There is no problem, water is reported to be full.
             }
-            CoolantLevel::Low => todo!(), // it depends
+            CoolantLevel::Low => {
+                match level_potential_problem {
+                    Some(problem) => {
+                        // Low coolant level was a problem before and still is a problem.
+                        // Check how long it has been a potential problem.
+                        if ctx.now.wrapping_sub(problem.since) > 500 {
+                            // Has been a potential problem long enough to now be an actual problem.
+                            ctx.problems
+                                .push(MachineProblem {
+                                    kind: ProblemKind::CoolantLevelInsufficient,
+                                    severity: ProblemSeverity::Critical,
+                                })
+                                .unwrap();
+                        } else {
+                            // Not long enough to be too concerned, yet...
+                            // Just continue with the existing potential problem.
+                            ctx.potential_problems.push(problem.clone()).unwrap();
+                        }
+                    }
+                    None => {
+                        // Low coolant level was not a problem before but now is.
+                        // Add a new potential problem.
+                        ctx.potential_problems
+                            .push(PotentialMachineProblem {
+                                problem: MachineProblem {
+                                    kind: ProblemKind::CoolantLevelInsufficient,
+                                    severity: ProblemSeverity::Critical,
+                                },
+                                since: ctx.now,
+                            })
+                            .unwrap();
+                    }
+                }
+            }
             CoolantLevel::CriticallyLow => {
                 // The coolant tank is effectively (< a few inches) empty.
                 // Add an active problem with critical severity.
@@ -54,6 +91,8 @@ fn coolant_level(ctx: &mut RuleEvaluationContext) {
                     .unwrap();
             }
         },
-        None => todo!(), // it depends
+        None => {
+            // TODO
+        }
     }
 }
