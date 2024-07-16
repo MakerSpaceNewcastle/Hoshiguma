c5e7ea32ebd1a19abe744839e6a33a542994af24
Dan Nixon <dan@dan-nixon.com>
2024-07-16 17:12:07 +0100

    wip

--- satori/firmware/src/rules/mod.rs
+++ satori/firmware/src/rules/mod.rs
@@ -1,5 +1,5 @@
 use hoshiguma_foundational_data::{
-    satori::{MachineProblems, ObservedState, PotentialMachineProblems},
+    satori::{CoolantLevel, MachineProblems, ObservedState, PotentialMachineProblems},
     TimeMillis,
 };
 
@@ -13,10 +13,27 @@ pub(super) struct RuleEvaluationContext<'a> {
 
 pub(super) fn evaluate(ctx: &RuleEvaluationContext) {
     coolant_level(ctx);
-    // TODO
+    // TODO: coolant flow
+    // TODO: pump RPM
+    // TODO: temperatures
+    // TODO: sensor fault
 }
 
 /// Ensure that the coolant level is sufficient.
+///
+/// Resolutions:
+///     - full: no problems
+///     - low: potential problem, promote to problem in 5 minutes
+///     - empty: immediate problem
+///     - unknown: potential problem, promote to problem in 5 minutes
 fn coolant_level(ctx: &RuleEvaluationContext) {
     // TODO
+    match &ctx.state.coolant_level {
+        Some(level) => match level {
+            CoolantLevel::Full => todo!(),
+            CoolantLevel::Low => todo!(),
+            CoolantLevel::CriticallyLow => todo!(),
+        },
+        None => todo!(),
+    }
 }
