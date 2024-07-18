3e09cb9aecb616ca5bc6d7dceae8469b005dd5d5
Dan Nixon <dan@dan-nixon.com>
2024-07-18 15:29:28 +0100

    wip

--- satori/firmware/src/main.rs
+++ satori/firmware/src/main.rs
@@ -2,6 +2,7 @@
 #![no_main]
 
 mod sensors;
+mod rules;
 
 use bsp::{
     entry,
@@ -15,10 +16,15 @@ use bsp::{
 use cortex_m::delay::Delay;
 use defmt::info;
 use defmt_rtt as _;
-use embedded_hal::digital::OutputPin;
+use embedded_hal::digital::{OutputPin, PinState, StatefulOutputPin};
+use heapless::Vec;
+use hoshiguma_foundational_data::satori::{ObservedState, Status};
+use one_wire_bus::OneWire;
 #[cfg(feature = "panic-probe")]
 use panic_probe as _;
 use rp_pico as bsp;
+
+use crate::rules::RuleEvaluationContext;
 
 #[cfg(not(feature = "panic-probe"))]
 #[panic_handler]
@@ -57,26 +63,91 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
 
-    let mut led_pin = pins.led.into_push_pull_output();
+    let mut led = pins.led.into_push_pull_output();
+
+    // TODO: serial
+
+    // TODO
+    let mut machine_enable = pins.gpio10.into_push_pull_output_in_state(PinState::Low);
+
+    let mut coolant_level_sensor = {
+        // TODO
+        let top = pins.gpio11.into_pull_up_input();
+        // TODO
+        let bottom = pins.gpio12.into_pull_up_input();
+        sensors::CoolantLevelSensor::new(top, bottom)
+    };
+
+    let mut onewire_bus = {
+        let pin = pins.gpio13.into_readable_output();
+        OneWire::new(pin).unwrap()
+    };
+
+    for device_address in onewire_bus.devices(false, &mut delay) {
+        let device_address = device_address.unwrap();
+        info!("Found one wire device at address: {:?}", device_address.0);
+    }
+
+    let mut temperature_sensors = crate::sensors::TemperatureSensors::new(onewire_bus, delay);
+    
+    let mut iteration_id: u32 = 0;
+    let mut last_potential_problems = Vec::new();
 
     loop {
-        info!("on!");
-        led_pin.set_high().unwrap();
-        delay.delay_ms(100);
-        info!("off!");
-        led_pin.set_low().unwrap();
-        delay.delay_ms(500);
+        // TODO
+        let now = 0;
 
-        panic!("test");
+        // TODO
+        let coolant_pump_rpm = 0.0;
+
+        // TODO
+        let coolant_flow_rate = 0.0;
+
+        let temperature = temperature_sensors.read();
+        let coolant_level = coolant_level_sensor.read();
+
+        let observed = ObservedState {
+            temperature,
+            coolant_level,
+            coolant_pump_rpm,
+            coolant_flow_rate,
+        };
+
+        let mut potential_problems = Vec::new();
+        let mut problems = Vec::new();
+
+        crate::rules::evaluate(RuleEvaluationContext {
+            state: &observed,
+            now,
+            last_potential_problems: &last_potential_problems,
+            potential_problems: &mut potential_problems,
+            problems: &mut problems,
+        });
+
+        let status = Status {
+            observed,
+            potential_problems,
+            problems,
+        };
+
+        // Allow the machine to operate when there are no problems, otherwise disable it
+        machine_enable
+            .set_state(match status.problems.is_empty() {
+                true => PinState::High,
+                false => PinState::Low,
+            })
+            .unwrap();
+
+        // TODO
+        // telemetry::status(&mut serial, now, iteration_id, &status);
+
+        led.toggle().unwrap();
+        // hal::Delay::new().delay_ms(250u16);
+
+        iteration_id = iteration_id.wrapping_add(1);
+        last_potential_problems = status.potential_problems;
     }
 }
-
-// use atmega_hal::prelude::*;
-// use embedded_hal::digital::{OutputPin, PinState};
-// use heapless::Vec;
-// use hoshiguma_foundational_data::satori::{ObservedState, Status};
-// use one_wire_bus::OneWire;
-// use rules::RuleEvaluationContext;
 
 // #[panic_handler]
 // fn panic(info: &core::panic::PanicInfo) -> ! {
@@ -99,122 +170,5 @@ fn main() -> ! {
 //     loop {
 //         led.toggle();
 //         hal::Delay::new().delay_ms(50u16);
-//     }
-// }
-
-// #[avr_device::entry]
-// fn main() -> ! {
-//     let dp = atmega_hal::Peripherals::take().unwrap();
-//     let pins = hal::Pins::with_mcu_pins(atmega_hal::pins!(dp));
-
-//     hal::millis_init(dp.TC0);
-//     unsafe { avr_device::interrupt::enable() };
-
-//     let mut serial = serial!(dp, pins, 57600);
-//     telemetry::boot(&mut serial);
-
-//     let mut machine_enable = pins.machine_enable.into_output();
-
-//     let mut coolant_level_sensor = {
-//         let top = pins.rj45_pin5.into_pull_up_input();
-//         let bottom = pins.rj45_pin6.into_pull_up_input();
-//         sensors::CoolantLevelSensor::new(top, bottom)
-//     };
-
-//     let _coolant_flow_sensor = pins.rj45_pin3.into_pull_up_input();
-//     let _coolant_pump_speed_sensor = pins.rj45_pin4.into_pull_up_input();
-
-//     // Enable the PCINT0 and PCINT2 interrupts
-//     // See datasheet: 12.2.4 PCICR - Pin Change Interrupt Control Register
-//     dp.EXINT.pcicr.write(|w| unsafe { w.bits(0b00000101) });
-
-//     // Enable pin change interrupts on PCINT23 which is pin PD7 (= d7)
-//     // See datasheet: 13.3.3 Alternate Functions of Port D
-//     dp.EXINT.pcmsk2.write(|w| w.bits(0b10000000));
-
-//     // Enable pin change interrupts on PCINT0 which is pin PB0 (= d8)
-//     // See datasheet: 13.3.1 Alternate Functions of Port B
-//     dp.EXINT.pcmsk0.write(|w| w.bits(0b00000001));
-
-//     let mut delay = hal::Delay::new();
-
-//     let mut onewire_bus = {
-//         let pin = pins.rj45_pin2.into_opendrain();
-//         OneWire::new(pin).unwrap()
-//     };
-
-//     for device_address in onewire_bus.devices(false, &mut delay) {
-//         let device_address = device_address.unwrap();
-//         telemetry::found_onewire_device(&mut serial, device_address.0);
-//     }
-
-//     let mut temperature_sensors = crate::sensors::TemperatureSensors::new(onewire_bus, delay);
-
-//     let mut led = pins.led.into_output();
-
-//     let mut iteration_id: u32 = 0;
-//     let mut last_potential_problems = Vec::new();
-
-//     loop {
-//         let now = crate::hal::millis();
-
-//         let count_0 = avr_device::interrupt::free(|_| unsafe {
-//             let count = COUNT_0;
-//             COUNT_0 = 0;
-//             count
-//         });
-//         // TODO
-//         let coolant_pump_rpm = count_0;
-
-//         let count_2 = avr_device::interrupt::free(|_| unsafe {
-//             let count = COUNT_2;
-//             COUNT_2 = 0;
-//             count
-//         });
-//         // TODO
-//         let coolant_flow_rate = count_2;
-
-//         let temperature = temperature_sensors.read();
-//         let coolant_level = coolant_level_sensor.read();
-
-//         let observed = ObservedState {
-//             temperature,
-//             coolant_level,
-//             coolant_pump_rpm,
-//             coolant_flow_rate,
-//         };
-
-//         let mut potential_problems = Vec::new();
-//         let mut problems = Vec::new();
-
-//         crate::rules::evaluate(RuleEvaluationContext {
-//             state: &observed,
-//             now,
-//             last_potential_problems: &last_potential_problems,
-//             potential_problems: &mut potential_problems,
-//             problems: &mut problems,
-//         });
-
-//         let status = Status {
-//             observed,
-//             potential_problems,
-//             problems,
-//         };
-
-//         // Allow the machine to operate when there are no problems, otherwise disable it
-//         machine_enable
-//             .set_state(match status.problems.is_empty() {
-//                 true => PinState::High,
-//                 false => PinState::Low,
-//             })
-//             .unwrap();
-
-//         telemetry::status(&mut serial, now, iteration_id, &status);
-
-//         led.toggle();
-//         hal::Delay::new().delay_ms(250u16);
-
-//         iteration_id = iteration_id.wrapping_add(1);
-//         last_potential_problems = status.potential_problems;
 //     }
 // }
