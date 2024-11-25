15157ea340c2744454c47d5d042afa36f77524a4
Dan Nixon <dan@dan-nixon.com>
2024-11-25 18:08:52 +0000

    Add heartbeat telemetry message

--- peripheral-controller/firmware/src/telemetry.rs
+++ peripheral-controller/firmware/src/telemetry.rs
@@ -4,9 +4,10 @@ use defmt::{debug, error, unwrap};
 use embassy_executor::Spawner;
 use embassy_rp::{peripherals::UART0, uart::Async, uart::UartTx};
 use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, channel::Channel};
+use embassy_time::{Duration, Ticker};
 use hoshiguma_telemetry_protocol::{
     payload::{
-        system::{Boot, SystemMessagePayload},
+        system::{Info, SystemMessagePayload},
         Payload,
     },
     Message,
@@ -56,27 +57,29 @@ fn new_message(payload: Payload) -> Message {
     }
 }
 
-pub(super) async fn report_boot(uart: &mut TelemetryUart) {
-    let msg = new_message(Payload::System(SystemMessagePayload::Boot(Boot {
-        name: "peripheral-controller".try_into().unwrap(),
+fn info_message() -> Info {
+    Info {
         #[cfg(feature = "git-version")]
         git_revision: git_version::git_version!().try_into().unwrap(),
         #[cfg(not(feature = "git-version"))]
         git_revision: "unknown".try_into().unwrap(),
-    })));
+    }
+}
 
+pub(super) async fn report_boot(uart: &mut TelemetryUart) {
+    let msg = new_message(Payload::System(SystemMessagePayload::Boot(info_message())));
     tx_message(uart, &msg).await;
 }
 
 #[cfg(not(feature = "panic-probe"))]
 pub(super) fn report_panic(uart: &mut TelemetryUart, info: &PanicInfo<'_>) {
     let msg = new_message(Payload::System(SystemMessagePayload::Panic(info.into())));
-
     tx_message_blocking(uart, &msg);
 }
 
 pub(super) fn spawn(spawner: Spawner, uart: TelemetryUart) {
     unwrap!(spawner.spawn(telemetry_tx_task(uart)));
+    unwrap!(spawner.spawn(heartbeat_task()));
 }
 
 static TELEMETRY_MESSAGES: Channel<CriticalSectionRawMutex, Message, 32> = Channel::new();
@@ -86,6 +89,20 @@ pub(super) async fn telemetry_tx_task(mut uart: TelemetryUart) {
     loop {
         let msg = TELEMETRY_MESSAGES.receive().await;
         tx_message(&mut uart, &msg).await;
+    }
+}
+
+#[embassy_executor::task]
+pub(super) async fn heartbeat_task() {
+    let mut ticker = Ticker::every(Duration::from_secs(15));
+
+    loop {
+        ticker.next().await;
+
+        let msg = new_message(Payload::System(SystemMessagePayload::Heartbeat(
+            info_message(),
+        )));
+        TELEMETRY_MESSAGES.send(msg).await;
     }
 }
 
--- peripheral-controller/telemetry-protocol/src/payload/system/info.rs
+++ peripheral-controller/telemetry-protocol/src/payload/system/info.rs
@@ -3,7 +3,6 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 #[allow(dead_code)]
-pub struct Boot {
-    pub name: TelemString,
+pub struct Info {
     pub git_revision: TelemString,
 }
