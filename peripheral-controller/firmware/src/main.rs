86968dd0cf23e3584bf6d9240bd7c48f7250e916
Dan Nixon <dan@dan-nixon.com>
2025-02-25 12:20:42 +0000

    peripheral-controller: add CPU usage measurement

--- peripheral-controller/firmware/src/main.rs
+++ peripheral-controller/firmware/src/main.rs
@@ -9,19 +9,20 @@ mod maybe_timer;
 mod telemetry;
 
 use assign_resources::assign_resources;
-use defmt::unwrap;
+use core::sync::atomic::Ordering;
+use defmt::{info, unwrap};
 use defmt_rtt as _;
-use embassy_executor::Executor;
+use embassy_executor::raw::Executor;
 use embassy_rp::{
     gpio::{Input, Level, Output, Pull},
     multicore::{spawn_core1, Stack},
     peripherals,
     watchdog::Watchdog,
 };
-use embassy_time::{Duration, Timer};
+use embassy_time::{Duration, Instant, Ticker, Timer};
 #[cfg(feature = "panic-probe")]
 use panic_probe as _;
-use portable_atomic as _;
+use portable_atomic::AtomicU64;
 use static_cell::StaticCell;
 use telemetry::TelemetryUart;
 
@@ -62,10 +63,6 @@ fn panic(info: &core::panic::PanicInfo) -> ! {
         embassy_time::block_for(Duration::from_millis(50));
     }
 }
-
-static mut CORE1_STACK: Stack<4096> = Stack::new();
-static EXECUTOR0: StaticCell<Executor> = StaticCell::new();
-static EXECUTOR1: StaticCell<Executor> = StaticCell::new();
 
 assign_resources! {
     status: StatusResources {
@@ -118,6 +115,14 @@ assign_resources! {
     },
 }
 
+static mut CORE_1_STACK: Stack<4096> = Stack::new();
+
+static EXECUTOR_0: StaticCell<Executor> = StaticCell::new();
+static EXECUTOR_1: StaticCell<Executor> = StaticCell::new();
+
+static SLEEP_TICKS_CORE_0: AtomicU64 = AtomicU64::new(0);
+static SLEEP_TICKS_CORE_1: AtomicU64 = AtomicU64::new(0);
+
 #[cortex_m_rt::entry]
 fn main() -> ! {
     let p = embassy_rp::init(Default::default());
@@ -135,62 +140,81 @@ fn main() -> ! {
     // Safety critical things go on core 1
     spawn_core1(
         p.CORE1,
-        unsafe { &mut *core::ptr::addr_of_mut!(CORE1_STACK) },
+        unsafe { &mut *core::ptr::addr_of_mut!(CORE_1_STACK) },
         move || {
-            let executor1 = EXECUTOR1.init(Executor::new());
-            executor1.run(|spawner| {
-                unwrap!(spawner.spawn(watchdog_feed_task(r.status)));
+            let executor_1 = EXECUTOR_1.init(Executor::new(usize::MAX as *mut ()));
+            let spawner = executor_1.spawner();
 
-                unwrap!(spawner.spawn(devices::machine_power_detector::task(
-                    r.machine_power_detect
-                )));
-                unwrap!(spawner.spawn(devices::digital_inputs::task(
-                    r.chassis_intrusion_detect,
-                    r.air_assist_demand_detect,
-                    r.machine_run_detect,
-                    r.fume_extraction_mode_switch,
-                    r.coolant_resevoir_level_sensor,
-                )));
+            unwrap!(spawner.spawn(watchdog_feed_task(r.status)));
 
-                // State monitor tasks
-                unwrap!(spawner.spawn(logic::safety::monitor::chassis_intrusion::task()));
+            unwrap!(spawner.spawn(devices::machine_power_detector::task(
+                r.machine_power_detect
+            )));
+            unwrap!(spawner.spawn(devices::digital_inputs::task(
+                r.chassis_intrusion_detect,
+                r.air_assist_demand_detect,
+                r.machine_run_detect,
+                r.fume_extraction_mode_switch,
+                r.coolant_resevoir_level_sensor,
+            )));
 
-                // State monitor observation and alarm tasks
-                unwrap!(spawner.spawn(logic::safety::alarms::monitor_observation_task()));
-                unwrap!(spawner.spawn(logic::safety::lockout::alarm_evaluation_task()));
+            // State monitor tasks
+            unwrap!(spawner.spawn(logic::safety::monitor::chassis_intrusion::task()));
 
-                // Machine operation permission control tasks
-                unwrap!(spawner.spawn(logic::safety::lockout::machine_lockout_task()));
-                unwrap!(spawner.spawn(devices::laser_enable::task(r.laser_enable)));
-                unwrap!(spawner.spawn(devices::machine_enable::task(r.machine_enable)));
+            // State monitor observation and alarm tasks
+            unwrap!(spawner.spawn(logic::safety::alarms::monitor_observation_task()));
+            unwrap!(spawner.spawn(logic::safety::lockout::alarm_evaluation_task()));
 
-                // Fume extraction control tasks
-                unwrap!(spawner.spawn(logic::fume_extraction::task()));
-                unwrap!(spawner.spawn(devices::fume_extraction_fan::task(r.fume_extraction_fan)));
-            });
+            // Machine operation permission control tasks
+            unwrap!(spawner.spawn(logic::safety::lockout::machine_lockout_task()));
+            unwrap!(spawner.spawn(devices::laser_enable::task(r.laser_enable)));
+            unwrap!(spawner.spawn(devices::machine_enable::task(r.machine_enable)));
+
+            // Fume extraction control tasks
+            unwrap!(spawner.spawn(logic::fume_extraction::task()));
+            unwrap!(spawner.spawn(devices::fume_extraction_fan::task(r.fume_extraction_fan)));
+
+            loop {
+                let before = Instant::now().as_ticks();
+                cortex_m::asm::wfe();
+                let after = Instant::now().as_ticks();
+                SLEEP_TICKS_CORE_1.fetch_add(after - before, Ordering::Relaxed);
+                unsafe { executor_1.poll() };
+            }
         },
     );
 
     // Everything else goes on core 0
-    let executor0 = EXECUTOR0.init(Executor::new());
-    executor0.run(|spawner| {
-        unwrap!(spawner.spawn(logic::status_lamp::task()));
-        unwrap!(spawner.spawn(devices::status_lamp::task(r.status_lamp)));
+    let executor_0 = EXECUTOR_0.init(Executor::new(usize::MAX as *mut ()));
+    let spawner = executor_0.spawner();
 
-        unwrap!(spawner.spawn(devices::temperature_sensors::task(r.onewire)));
+    unwrap!(spawner.spawn(logic::status_lamp::task()));
+    unwrap!(spawner.spawn(devices::status_lamp::task(r.status_lamp)));
 
-        // State monitor tasks
-        unwrap!(spawner.spawn(logic::safety::monitor::power::task()));
-        unwrap!(spawner.spawn(logic::safety::monitor::coolant_level::task()));
-        unwrap!(spawner.spawn(logic::safety::monitor::temperatures::task()));
+    unwrap!(spawner.spawn(devices::temperature_sensors::task(r.onewire)));
 
-        // Air assist control tasks
-        unwrap!(spawner.spawn(devices::air_assist::pump_task(r.air_assist_pump)));
-        unwrap!(spawner.spawn(logic::air_assist::task()));
+    // State monitor tasks
+    unwrap!(spawner.spawn(logic::safety::monitor::power::task()));
+    unwrap!(spawner.spawn(logic::safety::monitor::coolant_level::task()));
+    unwrap!(spawner.spawn(logic::safety::monitor::temperatures::task()));
 
-        // Telemetry reporting tasks
-        crate::telemetry::spawn(spawner, telemetry_uart);
-    });
+    // Air assist control tasks
+    unwrap!(spawner.spawn(devices::air_assist::pump_task(r.air_assist_pump)));
+    unwrap!(spawner.spawn(logic::air_assist::task()));
+
+    // Telemetry reporting tasks
+    crate::telemetry::spawn(spawner, telemetry_uart);
+
+    // CPU usage reporting
+    unwrap!(spawner.spawn(report_cpu_usage()));
+
+    loop {
+        let before = Instant::now().as_ticks();
+        cortex_m::asm::wfe();
+        let after = Instant::now().as_ticks();
+        SLEEP_TICKS_CORE_0.fetch_add(after - before, Ordering::Relaxed);
+        unsafe { executor_0.poll() };
+    }
 }
 
 #[embassy_executor::task]
@@ -204,5 +228,41 @@ async fn watchdog_feed_task(r: StatusResources) {
         watchdog.feed();
         onboard_led.toggle();
         Timer::after_millis(500).await;
+    }
+}
+
+#[embassy_executor::task]
+async fn report_cpu_usage() {
+    let mut previous_tick = 0u64;
+    let mut previous_sleep_tick_core_0 = 0u64;
+    let mut previous_sleep_tick_core_1 = 0u64;
+
+    let mut ticker = Ticker::every(Duration::from_secs(1));
+
+    loop {
+        ticker.next().await;
+
+        let current_tick = Instant::now().as_ticks();
+        let tick_difference = (current_tick - previous_tick) as f32;
+
+        let current_sleep_tick_core_0 = SLEEP_TICKS_CORE_0.load(Ordering::Relaxed);
+        let current_sleep_tick_core_1 = SLEEP_TICKS_CORE_1.load(Ordering::Relaxed);
+
+        let calc_cpu_usage = |current_sleep_tick: u64, previous_sleep_tick: u64| -> f32 {
+            let sleep_tick_difference = (current_sleep_tick - previous_sleep_tick) as f32;
+            1f32 - sleep_tick_difference / tick_difference
+        };
+
+        let usage_core_0 = calc_cpu_usage(current_sleep_tick_core_0, previous_sleep_tick_core_0);
+        let usage_core_1 = calc_cpu_usage(current_sleep_tick_core_1, previous_sleep_tick_core_1);
+
+        previous_tick = current_tick;
+        previous_sleep_tick_core_0 = current_sleep_tick_core_0;
+        previous_sleep_tick_core_1 = current_sleep_tick_core_1;
+
+        info!(
+            "Usage: core 0 = {}, core 1 = {}",
+            usage_core_0, usage_core_1
+        );
     }
 }
