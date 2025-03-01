9fb6c52e0a202a9807950bae7d0b2001dff5ccfd
Dan Nixon <dan@dan-nixon.com>
2025-03-01 16:11:41 +0000

    wip

--- protocol/src/lib.rs
+++ protocol/src/lib.rs
@@ -15,29 +15,34 @@ pub type Vec<T, const N: usize> = std::vec::Vec<T>;
 pub type Vec<T, const N: usize> = heapless::Vec<T, N>;
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct Payload {
-    pub millis_since_boot: u64,
-    pub msg: Message,
+pub enum Message<RPC, PUB> {
+    Rpc(RPC),
+    Publish { millis_since_boot: u64, msg: PUB },
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
-pub enum Message {
-    Rpc(controller::Rpc),
-    Publish,
-}
-
-#[derive(Debug, Serialize, Deserialize, Clone)]
-pub enum RpcMessage<REQ, RES> {
+pub enum Rpc<REQ, RES> {
     Request(REQ),
     Response(RES),
+}
+
+mod common {
+    pub type Ping = super::Rpc<u32, u32>;
+    pub type GetVersion = super::Rpc<(), crate::String<16>>;
+    pub type GetUptime = super::Rpc<(), u64>;
+    pub type Reset = super::Rpc<(), ()>;
 }
 
 mod controller {
     use serde::{Deserialize, Serialize};
 
+    pub type ControllerMessage = super::Message<Rpc, ()>;
+
     #[derive(Debug, Serialize, Deserialize, Clone)]
     pub enum Rpc {
-        Ping(super::RpcMessage<u32, u32>),
-        GetVersion(super::RpcMessage<(), crate::String<16>>),
+        Ping(crate::common::Ping),
+        GetVersion(crate::common::GetVersion),
+        GetUptime(crate::common::GetUptime),
+        Reset(crate::common::Reset),
     }
 }
