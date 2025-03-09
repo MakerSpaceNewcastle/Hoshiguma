a64740a73dac4dcf5be8929020dc4bcf0d43559a
Dan Nixon <dan@dan-nixon.com>
2025-03-09 17:34:35 +0000

    wip

--- peripheral-controller-firmware/src/main.rs
+++ peripheral-controller-firmware/src/main.rs
@@ -12,7 +12,7 @@ mod telemetry;
 use assign_resources::assign_resources;
 use cooler::CoolerUart;
 use core::sync::atomic::Ordering;
-use defmt::{info, unwrap};
+use defmt::{error, info, unwrap};
 use defmt_rtt as _;
 use embassy_executor::raw::Executor;
 use embassy_rp::{
@@ -21,7 +21,7 @@ use embassy_rp::{
     peripherals,
     watchdog::Watchdog,
 };
-use embassy_time::{Duration, Instant, Ticker, Timer};
+use embassy_time::{Duration, Instant, Ticker, Timer, WithTimeout};
 use git_version::git_version;
 #[cfg(feature = "panic-probe")]
 use panic_probe as _;
@@ -156,99 +156,12 @@ fn main() -> ! {
 
     info!("Version: {}", git_version!());
 
-    // Unused IO
-    let _in0 = Input::new(p.PIN_15, Pull::Down);
-    let _relay5 = Output::new(p.PIN_19, Level::Low);
-
-    let mut telemetry_uart: TelemetryUart = r.telemetry.into();
-    crate::telemetry::report_boot(&mut telemetry_uart);
-
-    // Safety critical things go on core 1
-    spawn_core1(
-        p.CORE1,
-        unsafe { &mut *core::ptr::addr_of_mut!(CORE_1_STACK) },
-        move || {
-            let executor_1 = EXECUTOR_1.init(Executor::new(usize::MAX as *mut ()));
-            let spawner = executor_1.spawner();
-
-            unwrap!(spawner.spawn(watchdog_feed_task(r.status)));
-
-            unwrap!(spawner.spawn(devices::machine_power_detector::task(
-                r.machine_power_detect
-            )));
-            unwrap!(spawner.spawn(devices::machine_run_detector::task(r.machine_run_detect)));
-            unwrap!(spawner.spawn(devices::chassis_intrusion_detector::task(
-                r.chassis_intrusion_detect
-            )));
-
-            // State monitor tasks
-            unwrap!(spawner.spawn(logic::safety::monitor::chassis_intrusion::task()));
-
-            // State monitor observation and alarm tasks
-            unwrap!(spawner.spawn(logic::safety::alarms::monitor_observation_task()));
-            unwrap!(spawner.spawn(logic::safety::lockout::alarm_evaluation_task()));
-
-            // Machine operation permission control tasks
-            unwrap!(spawner.spawn(logic::safety::lockout::machine_lockout_task()));
-            unwrap!(spawner.spawn(devices::laser_enable::task(r.laser_enable)));
-            unwrap!(spawner.spawn(devices::machine_enable::task(r.machine_enable)));
-
-            #[cfg(feature = "test-panic-on-core-1")]
-            unwrap!(spawner.spawn(dummy_panic()));
-
-            loop {
-                let before = Instant::now().as_ticks();
-                cortex_m::asm::wfe();
-                let after = Instant::now().as_ticks();
-                SLEEP_TICKS_CORE_1.fetch_add(after - before, Ordering::Relaxed);
-                if !PANIC_HALT.load(Ordering::Relaxed) {
-                    unsafe { executor_1.poll() };
-                }
-            }
-        },
-    );
-
     // Everything else goes on core 0
     let executor_0 = EXECUTOR_0.init(Executor::new(usize::MAX as *mut ()));
     let spawner = executor_0.spawner();
 
-    unwrap!(spawner.spawn(logic::status_lamp::task()));
-    unwrap!(spawner.spawn(devices::status_lamp::task(r.status_lamp)));
-
-    unwrap!(spawner.spawn(devices::coolant_resevoir_level_sensor::task(
-        r.coolant_resevoir_level_sensor
-    )));
-    unwrap!(spawner.spawn(devices::temperature_sensors::task(r.onewire)));
-
-    // State monitor tasks
-    unwrap!(spawner.spawn(logic::safety::monitor::power::task()));
-    unwrap!(spawner.spawn(logic::safety::monitor::coolant_level::task()));
-    unwrap!(spawner.spawn(logic::safety::monitor::temperatures::task()));
-
-    // Air assist control tasks
-    unwrap!(spawner.spawn(devices::air_assist_demand_detector::task(
-        r.air_assist_demand_detect
-    )));
-    unwrap!(spawner.spawn(devices::air_assist_pump::task(r.air_assist_pump)));
-    unwrap!(spawner.spawn(logic::air_assist::task()));
-
-    // Fume extraction control tasks
-    unwrap!(spawner.spawn(devices::fume_extraction_mode_switch::task(
-        r.fume_extraction_mode_switch
-    )));
-    unwrap!(spawner.spawn(devices::fume_extraction_fan::task(r.fume_extraction_fan)));
-    unwrap!(spawner.spawn(logic::fume_extraction::task()));
-
-    // Telemetry reporting tasks
-    crate::telemetry::spawn(spawner, telemetry_uart);
-
-    // CPU usage reporting
-    unwrap!(spawner.spawn(report_cpu_usage()));
-
-    #[cfg(feature = "test-panic-on-core-0")]
-    unwrap!(spawner.spawn(dummy_panic()));
-
     // WIP
+    unwrap!(spawner.spawn(telem_uart_test(r.telemetry.into())));
     unwrap!(spawner.spawn(cooler_uart_test(r.cooler_communication.into())));
 
     loop {
@@ -262,64 +175,14 @@ fn main() -> ! {
     }
 }
 
+// WIP
 #[embassy_executor::task]
-async fn watchdog_feed_task(r: StatusResources) {
-    let mut onboard_led = Output::new(r.led, Level::Low);
-
-    let mut watchdog = Watchdog::new(r.watchdog);
-    watchdog.start(Duration::from_millis(600));
-
-    loop {
-        watchdog.feed();
-        onboard_led.toggle();
-        Timer::after_millis(500).await;
-    }
-}
-
-#[embassy_executor::task]
-async fn report_cpu_usage() {
-    let mut previous_tick = 0u64;
-    let mut previous_sleep_tick_core_0 = 0u64;
-    let mut previous_sleep_tick_core_1 = 0u64;
-
-    let mut ticker = Ticker::every(Duration::from_secs(1));
-
-    loop {
-        ticker.next().await;
-
-        let current_tick = Instant::now().as_ticks();
-        let tick_difference = (current_tick - previous_tick) as f32;
-
-        let current_sleep_tick_core_0 = SLEEP_TICKS_CORE_0.load(Ordering::Relaxed);
-        let current_sleep_tick_core_1 = SLEEP_TICKS_CORE_1.load(Ordering::Relaxed);
-
-        let calc_cpu_usage = |current_sleep_tick: u64, previous_sleep_tick: u64| -> f32 {
-            let sleep_tick_difference = (current_sleep_tick - previous_sleep_tick) as f32;
-            1f32 - sleep_tick_difference / tick_difference
-        };
-
-        let usage_core_0 = calc_cpu_usage(current_sleep_tick_core_0, previous_sleep_tick_core_0);
-        let usage_core_1 = calc_cpu_usage(current_sleep_tick_core_1, previous_sleep_tick_core_1);
-
-        previous_tick = current_tick;
-        previous_sleep_tick_core_0 = current_sleep_tick_core_0;
-        previous_sleep_tick_core_1 = current_sleep_tick_core_1;
-
-        info!(
-            "Usage: core 0 = {}, core 1 = {}",
-            usage_core_0, usage_core_1
-        );
-    }
-}
-
-#[embassy_executor::task]
-async fn dummy_panic() {
-    embassy_time::Timer::after_secs(5).await;
-    panic!("oh dear, how sad. nevermind...");
+async fn telem_uart_test(mut uart: TelemetryUart) {
+    // TODO
 }
 
 // WIP
 #[embassy_executor::task]
-async fn cooler_uart_test(uart: CoolerUart) {
+async fn cooler_uart_test(mut uart: CoolerUart) {
     // TODO
 }
