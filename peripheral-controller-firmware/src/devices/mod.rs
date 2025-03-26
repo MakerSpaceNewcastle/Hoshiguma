46effb922f93f17f065d730f4e07cc9eaa2a4c77
Dan Nixon <dan@dan-nixon.com>
2025-03-26 17:44:04 +0000

    peripheral-controller: add cooler UART and dummy echo

--- peripheral-controller-firmware/src/devices/mod.rs
+++ peripheral-controller-firmware/src/devices/mod.rs
@@ -2,6 +2,7 @@ pub(crate) mod air_assist_demand_detector;
 pub(crate) mod air_assist_pump;
 pub(crate) mod chassis_intrusion_detector;
 pub(crate) mod coolant_resevoir_level_sensor;
+pub(crate) mod cooler;
 pub(crate) mod fume_extraction_fan;
 pub(crate) mod fume_extraction_mode_switch;
 pub(crate) mod laser_enable;
