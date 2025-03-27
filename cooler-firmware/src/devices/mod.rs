20dfd6ef40900b059ec45fb82660719a1ca24c8a
Dan Nixon <dan@dan-nixon.com>
2025-03-27 09:16:27 +0000

    wip

--- cooler-firmware/src/devices/mod.rs
+++ cooler-firmware/src/devices/mod.rs
@@ -1,5 +1,7 @@
 pub(crate) mod compressor;
 pub(crate) mod coolant_pump;
+pub(crate) mod header_tank_level;
+pub(crate) mod heat_exchanger_level;
 pub(crate) mod radiator_fan;
 pub(crate) mod stirrer;
 pub(crate) mod temperature_sensors;
