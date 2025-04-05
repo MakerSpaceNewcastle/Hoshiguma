86125d1f569fac2f4917d10256fa62cc2935e822
Dan Nixon <dan@dan-nixon.com>
2025-04-05 14:19:10 +0100

    wip

--- peripheral-controller-firmware/src/logic/safety/monitor/coolant_level.rs
+++ peripheral-controller-firmware/src/logic/safety/monitor/coolant_level.rs
@@ -3,7 +3,6 @@ use crate::devices::cooler::{
     HEADER_TANK_COOLANT_LEVEL_CHANGED, HEAT_EXCHANGER_FLUID_LEVEL_CHANGED,
 };
 use defmt::unwrap;
-use embassy_futures::select::{select, Either};
 use hoshiguma_protocol::{
     cooler::types::{HeaderTankCoolantLevel, HeatExchangeFluidLevel},
     peripheral_controller::types::MonitorKind,
@@ -11,86 +10,89 @@ use hoshiguma_protocol::{
 };
 
 #[embassy_executor::task]
-pub(crate) async fn task() {
+pub(crate) async fn heat_exchanger_task() {
     let status_tx = unwrap!(NEW_MONITOR_STATUS.publisher());
 
-    let mut header_tank_level_rx = HEADER_TANK_COOLANT_LEVEL_CHANGED.receiver().unwrap();
-    let mut heat_exchanger_level_rx = HEAT_EXCHANGER_FLUID_LEVEL_CHANGED.receiver().unwrap();
+    let mut level_rx = HEAT_EXCHANGER_FLUID_LEVEL_CHANGED.receiver().unwrap();
 
-    let mut header_tank_sensor = ObservedSeverity::default();
-    let mut header_tank_level_low = ObservedSeverity::default();
-    let mut header_tank_level_high = ObservedSeverity::default();
-    let mut heat_exchanger_level = ObservedSeverity::default();
+    let mut level_low = ObservedSeverity::default();
 
     loop {
-        match select(
-            header_tank_level_rx.changed(),
-            heat_exchanger_level_rx.changed(),
-        )
-        .await
-        {
-            Either::First(header_tank_reading) => {
-                header_tank_sensor
-                    .update_and_async(
-                        match header_tank_reading {
-                            Ok(_) => Severity::Normal,
-                            Err(_) => Severity::Warn,
-                        },
-                        |severity| async {
-                            status_tx
-                                .publish((MonitorKind::CoolantHeaderTankLevelSensorFault, severity))
-                                .await;
-                        },
-                    )
-                    .await;
+        let reading = level_rx.changed().await;
 
-                if let Ok(reading) = header_tank_reading {
-                    header_tank_level_low
-                        .update_and_async(
-                            match reading {
-                                HeaderTankCoolantLevel::Empty => Severity::Warn,
-                                HeaderTankCoolantLevel::Normal => Severity::Normal,
-                                HeaderTankCoolantLevel::Full => Severity::Normal,
-                            },
-                            |severity| async {
-                                status_tx
-                                    .publish((MonitorKind::CoolantHeaderTankEmpty, severity))
-                                    .await;
-                            },
-                        )
+        level_low
+            .update_and_async(
+                match reading {
+                    HeatExchangeFluidLevel::Normal => Severity::Normal,
+                    HeatExchangeFluidLevel::Low => Severity::Critical,
+                },
+                |severity| async {
+                    status_tx
+                        .publish((MonitorKind::HeatExchangerFluidLow, severity))
                         .await;
+                },
+            )
+            .await;
+    }
+}
 
-                    header_tank_level_high
-                        .update_and_async(
-                            match reading {
-                                HeaderTankCoolantLevel::Empty => Severity::Normal,
-                                HeaderTankCoolantLevel::Normal => Severity::Normal,
-                                HeaderTankCoolantLevel::Full => Severity::Warn,
-                            },
-                            |severity| async {
-                                status_tx
-                                    .publish((MonitorKind::CoolantHeaderTankOverfilled, severity))
-                                    .await;
-                            },
-                        )
+#[embassy_executor::task]
+pub(crate) async fn coolant_header_tank_task() {
+    let status_tx = unwrap!(NEW_MONITOR_STATUS.publisher());
+
+    let mut level_rx = HEADER_TANK_COOLANT_LEVEL_CHANGED.receiver().unwrap();
+
+    let mut sensor = ObservedSeverity::default();
+    let mut level_low = ObservedSeverity::default();
+    let mut level_high = ObservedSeverity::default();
+
+    loop {
+        let reading = level_rx.changed().await;
+
+        sensor
+            .update_and_async(
+                match reading {
+                    Ok(_) => Severity::Normal,
+                    Err(_) => Severity::Warn,
+                },
+                |severity| async {
+                    status_tx
+                        .publish((MonitorKind::CoolantHeaderTankLevelSensorFault, severity))
                         .await;
-                }
-            }
-            Either::Second(heat_exchanger_reading) => {
-                heat_exchanger_level
-                    .update_and_async(
-                        match heat_exchanger_reading {
-                            HeatExchangeFluidLevel::Normal => Severity::Normal,
-                            HeatExchangeFluidLevel::Low => Severity::Critical,
-                        },
-                        |severity| async {
-                            status_tx
-                                .publish((MonitorKind::HeatExchangerFluidLow, severity))
-                                .await;
-                        },
-                    )
-                    .await;
-            }
+                },
+            )
+            .await;
+
+        if let Ok(reading) = reading {
+            level_low
+                .update_and_async(
+                    match reading {
+                        HeaderTankCoolantLevel::Empty => Severity::Warn,
+                        HeaderTankCoolantLevel::Normal => Severity::Normal,
+                        HeaderTankCoolantLevel::Full => Severity::Normal,
+                    },
+                    |severity| async {
+                        status_tx
+                            .publish((MonitorKind::CoolantHeaderTankEmpty, severity))
+                            .await;
+                    },
+                )
+                .await;
+
+            level_high
+                .update_and_async(
+                    match reading {
+                        HeaderTankCoolantLevel::Empty => Severity::Normal,
+                        HeaderTankCoolantLevel::Normal => Severity::Normal,
+                        HeaderTankCoolantLevel::Full => Severity::Warn,
+                    },
+                    |severity| async {
+                        status_tx
+                            .publish((MonitorKind::CoolantHeaderTankOverfilled, severity))
+                            .await;
+                    },
+                )
+                .await;
         }
     }
 }
--- peripheral-controller-firmware/src/main.rs
+++ peripheral-controller-firmware/src/main.rs
@@ -205,7 +205,8 @@ fn main() -> ! {
 
     // State monitor tasks
     unwrap!(spawner.spawn(logic::safety::monitor::power::task()));
-    unwrap!(spawner.spawn(logic::safety::monitor::coolant_level::task()));
+    unwrap!(spawner.spawn(logic::safety::monitor::coolant_level::heat_exchanger_task()));
+    unwrap!(spawner.spawn(logic::safety::monitor::coolant_level::coolant_header_tank_task()));
     unwrap!(spawner.spawn(logic::safety::monitor::temperatures::task()));
 
     // Air assist control tasks
