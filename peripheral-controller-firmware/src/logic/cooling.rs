45f5144ed88b2eec7fea2ba6a0c86ff38a47b314
Dan Nixon <dan@dan-nixon.com>
2025-04-05 13:00:12 +0100

    wip

--- peripheral-controller-firmware/src/logic/cooling.rs
+++ peripheral-controller-firmware/src/logic/cooling.rs
@@ -3,9 +3,10 @@ use crate::devices::{
     machine_power_detector::MACHINE_POWER_CHANGED,
 };
 use defmt::unwrap;
+use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, watch::Watch};
 use hoshiguma_protocol::{
-    cooler::types::{CoolantPump, Stirrer},
-    peripheral_controller::types::MachinePower,
+    cooler::types::{Compressor, CoolantPump, RadiatorFan, Stirrer},
+    peripheral_controller::types::{CoolingDemand, MachinePower},
 };
 
 #[embassy_executor::task]
@@ -32,6 +33,38 @@ pub(crate) async fn power_control() {
                     .await;
                 cooler_command_tx
                     .publish(CoolerControlCommand::SetStirrer(Stirrer::Idle))
+                    .await;
+            }
+        }
+    }
+}
+
+pub(crate) static COOLING_DEMAND: Watch<CriticalSectionRawMutex, CoolingDemand, 1> = Watch::new();
+
+#[embassy_executor::task]
+pub(crate) async fn cooling_control() {
+    let mut cooling_demand_rx = COOLING_DEMAND.receiver().unwrap();
+
+    let cooler_command_tx = unwrap!(COOLER_CONTROL.publisher());
+
+    loop {
+        let cooling_demand = cooling_demand_rx.changed().await;
+
+        match cooling_demand {
+            CoolingDemand::Demand => {
+                cooler_command_tx
+                    .publish(CoolerControlCommand::SetRadiatorFan(RadiatorFan::Run))
+                    .await;
+                cooler_command_tx
+                    .publish(CoolerControlCommand::SetCompressor(Compressor::Run))
+                    .await;
+            }
+            CoolingDemand::Idle => {
+                cooler_command_tx
+                    .publish(CoolerControlCommand::SetRadiatorFan(RadiatorFan::Idle))
+                    .await;
+                cooler_command_tx
+                    .publish(CoolerControlCommand::SetCompressor(Compressor::Idle))
                     .await;
             }
         }
