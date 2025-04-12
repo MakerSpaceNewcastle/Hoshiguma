7d021073ee997ba1fee0fafdc479b5f6285e779c
Dan Nixon <dan@dan-nixon.com>
2025-04-12 16:30:53 +0100

    peripheral-controller: use separate task lists for each core

--- peripheral-controller-firmware/src/trace.rs
+++ peripheral-controller-firmware/src/trace.rs
@@ -1,21 +1,21 @@
-use core::{cell::RefCell, fmt::Write, future::poll_fn, task::Poll};
-use defmt::{debug, info};
+use core::{cell::RefCell, fmt::Write, future::poll_fn, sync::atomic::Ordering, task::Poll};
+use defmt::{info, Format};
 use embassy_executor::raw::task_from_waker;
 use embassy_sync::blocking_mutex::CriticalSectionMutex;
 use embassy_time::{Duration, Instant, Ticker};
 use heapless::{FnvIndexMap, String};
+use portable_atomic::AtomicU32;
 
-#[derive(Debug, defmt::Format)]
+#[derive(Clone, Debug, Format)]
 enum TaskState {
     Idle,
     Running,
     Waiting,
 }
 
-#[derive(Debug, defmt::Format)]
+#[derive(Clone, Debug, Format)]
 struct TaskInfo {
     name: Option<&'static str>,
-    executor_id: u32,
     total_idle: Duration,
     total_running: Duration,
     total_waiting: Duration,
@@ -24,11 +24,10 @@ struct TaskInfo {
     current_state: TaskState,
 }
 
-impl TaskInfo {
-    fn new(executor_id: u32) -> Self {
+impl Default for TaskInfo {
+    fn default() -> Self {
         Self {
             name: None,
-            executor_id,
             total_idle: Duration::default(),
             total_running: Duration::default(),
             total_waiting: Duration::default(),
@@ -37,7 +36,9 @@ impl TaskInfo {
             current_state: TaskState::Idle,
         }
     }
+}
 
+impl TaskInfo {
     fn name(&self) -> &'static str {
         self.name.unwrap_or("unknown")
     }
@@ -70,26 +71,53 @@ impl TaskInfo {
     }
 }
 
-static TASKS: CriticalSectionMutex<RefCell<FnvIndexMap<u32, TaskInfo, 32>>> =
+static TASKS_CORE_0: CriticalSectionMutex<RefCell<FnvIndexMap<u32, TaskInfo, 32>>> =
     CriticalSectionMutex::new(RefCell::new(FnvIndexMap::new()));
-static EXECUTOR_NAMES: CriticalSectionMutex<RefCell<FnvIndexMap<u32, &'static str, 8>>> =
+static TASKS_CORE_1: CriticalSectionMutex<RefCell<FnvIndexMap<u32, TaskInfo, 32>>> =
     CriticalSectionMutex::new(RefCell::new(FnvIndexMap::new()));
+
+static CORE_0_EXECUTOR_ID: AtomicU32 = AtomicU32::new(0);
+static CORE_1_EXECUTOR_ID: AtomicU32 = AtomicU32::new(0);
+
+#[derive(Debug, Format)]
+enum Core {
+    Core0,
+    Core1,
+}
+
+fn get_core(executor_id: u32) -> Core {
+    if CORE_0_EXECUTOR_ID.load(Ordering::Relaxed) == executor_id {
+        Core::Core0
+    } else if CORE_1_EXECUTOR_ID.load(Ordering::Relaxed) == executor_id {
+        Core::Core1
+    } else {
+        panic!("Unknown executor id {}", executor_id);
+    }
+}
 
 #[unsafe(no_mangle)]
 unsafe extern "Rust" fn _embassy_trace_task_new(executor_id: u32, task_id: u32) {
     info!("New task {} executor {}", task_id, executor_id);
 
-    TASKS.lock(|tasks| {
+    match get_core(executor_id) {
+        Core::Core0 => &TASKS_CORE_0,
+        Core::Core1 => &TASKS_CORE_1,
+    }
+    .lock(|tasks| {
         tasks
             .borrow_mut()
-            .insert(task_id, TaskInfo::new(executor_id))
+            .insert(task_id, TaskInfo::default())
             .unwrap();
     });
 }
 
 #[unsafe(no_mangle)]
-unsafe extern "Rust" fn _embassy_trace_task_ready_begin(_executor_id: u32, task_id: u32) {
-    TASKS.lock(|tasks| {
+unsafe extern "Rust" fn _embassy_trace_task_ready_begin(executor_id: u32, task_id: u32) {
+    match get_core(executor_id) {
+        Core::Core0 => &TASKS_CORE_0,
+        Core::Core1 => &TASKS_CORE_1,
+    }
+    .lock(|tasks| {
         if let Some(task) = tasks.borrow_mut().get_mut(&task_id) {
             task.update(Instant::now(), TaskState::Waiting);
             task.total_wakes = task.total_wakes.saturating_add(1);
@@ -98,8 +126,12 @@ unsafe extern "Rust" fn _embassy_trace_task_ready_begin(_executor_id: u32, task_
 }
 
 #[unsafe(no_mangle)]
-unsafe extern "Rust" fn _embassy_trace_task_exec_begin(_executor_id: u32, task_id: u32) {
-    TASKS.lock(|tasks| {
+unsafe extern "Rust" fn _embassy_trace_task_exec_begin(executor_id: u32, task_id: u32) {
+    match get_core(executor_id) {
+        Core::Core0 => &TASKS_CORE_0,
+        Core::Core1 => &TASKS_CORE_1,
+    }
+    .lock(|tasks| {
         if let Some(task) = tasks.borrow_mut().get_mut(&task_id) {
             task.update(Instant::now(), TaskState::Running);
         }
@@ -107,8 +139,12 @@ unsafe extern "Rust" fn _embassy_trace_task_exec_begin(_executor_id: u32, task_i
 }
 
 #[unsafe(no_mangle)]
-unsafe extern "Rust" fn _embassy_trace_task_exec_end(_executor_id: u32, task_id: u32) {
-    TASKS.lock(|tasks| {
+unsafe extern "Rust" fn _embassy_trace_task_exec_end(executor_id: u32, task_id: u32) {
+    match get_core(executor_id) {
+        Core::Core0 => &TASKS_CORE_0,
+        Core::Core1 => &TASKS_CORE_1,
+    }
+    .lock(|tasks| {
         if let Some(task) = tasks.borrow_mut().get_mut(&task_id) {
             task.update(Instant::now(), TaskState::Idle);
         }
@@ -121,23 +157,26 @@ unsafe extern "Rust" fn _embassy_trace_poll_start(_executor_id: u32) {}
 #[unsafe(no_mangle)]
 unsafe extern "Rust" fn _embassy_trace_executor_idle(_executor_id: u32) {}
 
-pub(crate) fn name_executor(executor_id: u32, name: &'static str) {
-    EXECUTOR_NAMES.lock(|executor_names| {
-        executor_names
-            .borrow_mut()
-            .insert(executor_id, name)
-            .unwrap();
-    });
+pub(crate) fn identify_core_0_executor(executor_id: u32) {
+    CORE_0_EXECUTOR_ID.store(executor_id, Ordering::Relaxed);
+}
+
+pub(crate) fn identify_core_1_executor(executor_id: u32) {
+    CORE_1_EXECUTOR_ID.store(executor_id, Ordering::Relaxed);
 }
 
 pub(crate) async fn name_task(name: &'static str) {
+    assert!(name.len() <= 20, "Task name too long");
+
     let task_id: u32 = poll_fn(|cx| Poll::Ready(task_from_waker(cx.waker()).as_id())).await;
 
-    TASKS.lock(|tasks| {
-        if let Some(task) = tasks.borrow_mut().get_mut(&task_id) {
-            task.name = Some(name);
-        }
-    });
+    for task_list in [&TASKS_CORE_0, &TASKS_CORE_1] {
+        task_list.lock(|tasks| {
+            if let Some(task) = tasks.borrow_mut().get_mut(&task_id) {
+                task.name = Some(name);
+            }
+        });
+    }
 }
 
 #[embassy_executor::task]
@@ -146,50 +185,70 @@ pub(crate) async fn task() {
 
     let mut ticker = Ticker::every(Duration::from_secs(15));
 
-    let mut last_report_time = Instant::now();
     let mut table_line_buffer = String::<120>::new();
+
+    let mut last_report_time_core_0 = Instant::now();
+    let mut last_report_time_core_1 = Instant::now();
 
     loop {
         ticker.next().await;
 
-        info!("│Task        │Executor│State│Idle  │Run   │Wait  │#Wakes│");
-        info!("├────────────┼────────┼─────┼──────┼──────┼──────┼──────┤");
-        TASKS.lock(|tasks| {
+        let (core_0_tasks, core_0_ticks) = TASKS_CORE_0.lock(|tasks| {
             let now = Instant::now();
-            let ticks = (now.saturating_duration_since(last_report_time)).as_ticks();
-
+            let ticks = (now.saturating_duration_since(last_report_time_core_0)).as_ticks();
+            let sample = tasks.borrow().clone();
             for task in tasks.borrow_mut().values_mut() {
-                let executor_name = EXECUTOR_NAMES
-                    .lock(|executor_names| {
-                        executor_names.borrow_mut().get(&task.executor_id).copied()
-                    })
-                    .unwrap_or("unknown");
-
-                table_line_buffer
-                    .write_fmt(format_args!(
-                        "│{:<12}│{:<8}│{}│{:.4}│{:.4}│{:.4}│{:>6}│",
-                        task.name(),
-                        executor_name,
-                        match task.current_state {
-                            TaskState::Idle => "Idle ",
-                            TaskState::Running => "Run  ",
-                            TaskState::Waiting => "Wait ",
-                        },
-                        task.total_idle.as_ticks() as f32 / ticks as f32,
-                        task.total_running.as_ticks() as f32 / ticks as f32,
-                        task.total_waiting.as_ticks() as f32 / ticks as f32,
-                        task.total_wakes
-                    ))
-                    .unwrap();
-
                 task.reset_counters(now);
-
-                info!("{}", table_line_buffer);
-                table_line_buffer.clear();
             }
-
-            debug!("Ticks in interval: {}", ticks);
-            last_report_time = now;
+            last_report_time_core_0 = now;
+            (sample, ticks)
         });
+
+        let (core_1_tasks, core_1_ticks) = TASKS_CORE_1.lock(|tasks| {
+            let now = Instant::now();
+            let ticks = (now.saturating_duration_since(last_report_time_core_1)).as_ticks();
+            let sample = tasks.borrow().clone();
+            for task in tasks.borrow_mut().values_mut() {
+                task.reset_counters(now);
+            }
+            last_report_time_core_1 = now;
+            (sample, ticks)
+        });
+
+        info!("│Task                │Core│State│Idle  │Run   │Wait  │#Wakes│");
+        info!("├────────────────────┼────┼─────┼──────┼──────┼──────┼──────┤");
+
+        for task in core_0_tasks.values() {
+            print_task_line(task, Core::Core0, core_0_ticks, &mut table_line_buffer);
+        }
+
+        for task in core_1_tasks.values() {
+            print_task_line(task, Core::Core1, core_1_ticks, &mut table_line_buffer);
+        }
     }
+}
+
+fn print_task_line(task: &TaskInfo, core: Core, core_ticks: u64, buffer: &mut String<120>) {
+    buffer
+        .write_fmt(format_args!(
+            "│{:<20}│{}│{}│{:.4}│{:.4}│{:.4}│{:>6}│",
+            task.name(),
+            match core {
+                Core::Core0 => "0   ",
+                Core::Core1 => "1   ",
+            },
+            match task.current_state {
+                TaskState::Idle => "Idle ",
+                TaskState::Running => "Run  ",
+                TaskState::Waiting => "Wait ",
+            },
+            task.total_idle.as_ticks() as f32 / core_ticks as f32,
+            task.total_running.as_ticks() as f32 / core_ticks as f32,
+            task.total_waiting.as_ticks() as f32 / core_ticks as f32,
+            task.total_wakes
+        ))
+        .unwrap();
+
+    info!("{}", buffer);
+    buffer.clear();
 }
