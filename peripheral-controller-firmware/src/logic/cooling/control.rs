6d0832d916cbf4b6bb905e9fd3089efbe7804d2d
Dan Nixon <dan@dan-nixon.com>
2025-05-11 20:45:07 +0100

    peripheral-controller: split cooling control and demand tasks into separate files

--- peripheral-controller-firmware/src/logic/cooling/control.rs
+++ peripheral-controller-firmware/src/logic/cooling/control.rs
@@ -1,16 +1,15 @@
-use super::safety::monitor::MONITORS_CHANGED;
 use crate::{
     changed::ObservedValue,
     devices::{
-        cooler::{CoolerControlCommand, COOLER_CONTROL_COMMAND, COOLER_TEMPERATURES_READ},
+        cooler::{CoolerControlCommand, COOLER_CONTROL_COMMAND},
         machine_power_detector::MACHINE_POWER_CHANGED,
     },
+    logic::safety::monitor::MONITORS_CHANGED,
     telemetry::queue_telemetry_event,
 };
 use defmt::{info, unwrap};
 use embassy_futures::select::{select3, Either3};
 use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, pubsub::Publisher, watch::Watch};
-use embassy_time::{Duration, Instant};
 use hoshiguma_protocol::{
     cooler::types::{CompressorState, CoolantPumpState, RadiatorFanState},
     peripheral_controller::{
@@ -23,7 +22,7 @@ use hoshiguma_protocol::{
 pub(crate) static COOLING_DEMAND: Watch<CriticalSectionRawMutex, CoolingDemand, 1> = Watch::new();
 
 #[embassy_executor::task]
-pub(crate) async fn control_task() {
+pub(crate) async fn task() {
     #[cfg(feature = "trace")]
     crate::trace::name_task("cooler control logic").await;
 
@@ -143,69 +142,4 @@ async fn send_cooler_demand_command<const CAP: usize, const SUBS: usize, const P
         CoolingDemand::Demand => CompressorState::Run,
     }))
     .await;
-}
-
-#[embassy_executor::task]
-pub(crate) async fn demand_task() {
-    #[cfg(feature = "trace")]
-    crate::trace::name_task("cooler demand logic").await;
-
-    let mut temperatures_rx = COOLER_TEMPERATURES_READ.receiver().unwrap();
-
-    let cooling_demand_tx = COOLING_DEMAND.sender();
-
-    let mut demand = ObservedValue::new(CoolingDemand::Idle);
-    let mut last_state_change = None;
-
-    const UPPER_TEMPERATURE: f32 = 17.5;
-    const LOWER_TEMPERATURE: f32 = 17.0;
-
-    loop {
-        let temperatures = temperatures_rx.changed().await;
-
-        if let Ok(res_flow_loop_temperature) = temperatures.reservoir_left_side {
-            // Get the new demand based on temperature alone, with hysteresis
-            let new_demand = if res_flow_loop_temperature > UPPER_TEMPERATURE {
-                Some(CoolingDemand::Demand)
-            } else if res_flow_loop_temperature < LOWER_TEMPERATURE {
-                Some(CoolingDemand::Idle)
-            } else {
-                None
-            };
-            info!(
-                "Reservoir flow temperature {} calls for cooling demand {}",
-                res_flow_loop_temperature, new_demand
-            );
-
-            // Determine if the state should be allowed to change now based on elapsed time since
-            // the last commanded state change
-            let now = Instant::now();
-            let state_change_allowed = match last_state_change {
-                Some(time) => {
-                    // Send a demand change command at most every 60 seconds to avoid cycling the compressor
-                    // too often
-                    now.saturating_duration_since(time) >= Duration::from_secs(60)
-                }
-                None => {
-                    // If no previous command was sent then send the demand command immediately
-                    true
-                }
-            };
-            info!(
-                "Demand last changed at {}, now {}, change allowed: {}",
-                last_state_change, now, state_change_allowed
-            );
-
-            // If the final demand command is new, then send it
-            if let Some(new_demand) = new_demand {
-                if state_change_allowed {
-                    demand.update_and(new_demand, |demand| {
-                        info!("Cooling demand: {}", demand);
-                        cooling_demand_tx.send(demand);
-                        last_state_change = Some(now);
-                    });
-                }
-            }
-        }
-    }
 }
