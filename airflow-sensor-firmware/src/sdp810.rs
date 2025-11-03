cb22d824e395fa10aac258aec4c543068fc68f0b
Dan Nixon <dan@dan-nixon.com>
2025-11-03 20:57:50 +0000

    wip

--- airflow-sensor-firmware/src/sdp810.rs
+++ airflow-sensor-firmware/src/sdp810.rs
@@ -1,11 +1,12 @@
 use super::Sdp810Resources;
-use defmt::info;
+use defmt::{debug, info};
 use embassy_rp::{
     bind_interrupts,
     i2c::{Config, I2c, InterruptHandler},
     peripherals::I2C1,
 };
-use embassy_time::Timer;
+use embassy_time::{Duration, Ticker, Timer};
+use sensirion_i2c::i2c_async::{read_words_with_crc, write_command_u16, write_command_u8};
 
 bind_interrupts!(struct Irqs {
     I2C1_IRQ => InterruptHandler<I2C1>;
@@ -19,38 +20,41 @@ pub(super) async fn task(r: Sdp810Resources) -> ! {
 
     Timer::after_millis(100).await;
 
-    // Reset
-    sensirion_i2c::i2c_async::write_command_u8(&mut i2c, 0x00, 0x06).await.unwrap();
+    // Soft reset the device
+    write_command_u8(&mut i2c, 0x00, 0x06).await.unwrap();
 
     Timer::after_millis(100).await;
 
-    sensirion_i2c::i2c_async::write_command_u16(&mut i2c, DEVICE_ADDRESS, CMD_READ_PRODUCT_ID_1).await.unwrap();
-    sensirion_i2c::i2c_async::write_command_u16(&mut i2c, DEVICE_ADDRESS, CMD_READ_PRODUCT_ID_2).await.unwrap();
+    // Get sensor product data
+    write_command_u16(&mut i2c, DEVICE_ADDRESS, CMD_READ_PRODUCT_ID_1).await.unwrap();
+    write_command_u16(&mut i2c, DEVICE_ADDRESS, CMD_READ_PRODUCT_ID_2).await.unwrap();
 
     let mut buff = [0u8; 18];
-    sensirion_i2c::i2c_async::read_words_with_crc(&mut i2c, DEVICE_ADDRESS, &mut buff).await;
-    info!("got id bytes: {}", buff);
+    read_words_with_crc(&mut i2c, DEVICE_ADDRESS, &mut buff).await;
+    debug!("Got product ID bytes: {}", buff);
 
-    sensirion_i2c::i2c_async::write_command_u16(&mut i2c, DEVICE_ADDRESS, CMD_CONT_MASS_FLOW_AVG).await.unwrap();
+    write_command_u16(&mut i2c, DEVICE_ADDRESS, CMD_CONT_MASS_FLOW_AVG).await.unwrap();
+
+    let mut tick = Ticker::every(Duration::from_millis(100));
 
     loop {
-        Timer::after_millis(500).await;
-
         let mut buffer = [0u8; 9];
-        sensirion_i2c::i2c_async::read_words_with_crc(&mut i2c, DEVICE_ADDRESS, &mut buffer).await;
-        info!("got sample bytes: {}", buffer);
+        read_words_with_crc(&mut i2c, DEVICE_ADDRESS, &mut buffer).await;
+        debug!("Got measurement bytes: {}", buffer);
 
-        let dp_raw = i16::from(buffer[0]) << 8 | i16::from(buffer[1]);
-        let temp_raw = i16::from(buffer[3]) << 8 | i16::from(buffer[4]);
-        let dp_scale = i16::from(buffer[6]) << 8 | i16::from(buffer[7]);
+        let pressure = i16::from(buffer[0]) << 8 | i16::from(buffer[1]);
+        let temperature = i16::from(buffer[3]) << 8 | i16::from(buffer[4]);
+        let pressure_scale = i16::from(buffer[6]) << 8 | i16::from(buffer[7]);
 
-        info!("scale: {}", dp_scale);
+        debug!("Pressure scale: {}", pressure_scale);
 
-        let value = f32::from(dp_raw) / f32::from(dp_scale);
-        let temperature = f32::from(temp_raw) / TEMPERATURE_SCALE_FACTOR;
+        let pressure = f32::from(pressure) / f32::from(pressure_scale);
+        let temperature = f32::from(temperature) / TEMPERATURE_SCALE_FACTOR;
 
-        info!("pressure: {} Pa", value);
+        info!("pressure: {} Pa", pressure);
         info!("temperature: {} C", temperature);
+
+        tick.next().await;
     }
 }
 
