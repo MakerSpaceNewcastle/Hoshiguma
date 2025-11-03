6d4a6815412ca8c2126b28b48bc120d7bee31afb
Dan Nixon <dan@dan-nixon.com>
2025-11-03 19:27:23 +0000

    wip

--- airflow-sensor-firmware/src/sdp810.rs
+++ airflow-sensor-firmware/src/sdp810.rs
@@ -14,17 +14,22 @@ bind_interrupts!(struct Irqs {
 #[embassy_executor::task]
 pub(super) async fn task(r: Sdp810Resources) -> ! {
     let mut config = Config::default();
-    config.frequency = 100_000;
+    config.frequency = 400_000;
     let mut i2c = I2c::new_async(r.i2c, r.scl_pin, r.sda_pin, Irqs, config);
 
-    Timer::after_millis(500).await;
+    Timer::after_millis(100).await;
+
+    // Reset
+    sensirion_i2c::i2c_async::write_command_u8(&mut i2c, 0x00, 0x06).await.unwrap();
+
+    Timer::after_millis(100).await;
 
     sensirion_i2c::i2c_async::write_command_u16(&mut i2c, DEVICE_ADDRESS, CMD_READ_PRODUCT_ID_1).await.unwrap();
     sensirion_i2c::i2c_async::write_command_u16(&mut i2c, DEVICE_ADDRESS, CMD_READ_PRODUCT_ID_2).await.unwrap();
 
     let mut buff = [0u8; 18];
     sensirion_i2c::i2c_async::read_words_with_crc(&mut i2c, DEVICE_ADDRESS, &mut buff).await;
-    info!("got bytes: {}", buff);
+    info!("got id bytes: {}", buff);
 
     sensirion_i2c::i2c_async::write_command_u16(&mut i2c, DEVICE_ADDRESS, CMD_CONT_MASS_FLOW_AVG).await.unwrap();
 
@@ -33,7 +38,19 @@ pub(super) async fn task(r: Sdp810Resources) -> ! {
 
         let mut buffer = [0u8; 9];
         sensirion_i2c::i2c_async::read_words_with_crc(&mut i2c, DEVICE_ADDRESS, &mut buffer).await;
-        info!("got bytes: {}", buff);
+        info!("got sample bytes: {}", buffer);
+
+        let dp_raw = i16::from(buffer[0]) << 8 | i16::from(buffer[1]);
+        let temp_raw = i16::from(buffer[3]) << 8 | i16::from(buffer[4]);
+        let dp_scale = i16::from(buffer[6]) << 8 | i16::from(buffer[7]);
+
+        info!("scale: {}", dp_scale);
+
+        let value = f32::from(dp_raw) / f32::from(dp_scale);
+        let temperature = f32::from(temp_raw) / TEMPERATURE_SCALE_FACTOR;
+
+        info!("pressure: {} Pa", value);
+        info!("temperature: {} C", temperature);
     }
 }
 
@@ -45,3 +62,5 @@ const CMD_CONT_MASS_FLOW_AVG: u16 = 0x3603;
 
 const CMD_READ_PRODUCT_ID_1: u16 = 0x367C;
 const CMD_READ_PRODUCT_ID_2: u16 = 0xE102;
+
+const TEMPERATURE_SCALE_FACTOR: f32 = 200.0f32;
