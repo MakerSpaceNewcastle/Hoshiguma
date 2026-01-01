053fb411f5b13879747e150ebbc202179606a380
Dan Nixon <dan@dan-nixon.com>
2026-01-01 17:42:03 +0000

    Replace devenv.sh with simple Nix flake

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        # Code formatting tools
+        treefmt
+        alejandra
+        mdl
+        rustfmt
+
+        # Rust toolchain
+        rustup
+        probe-rs-tools
+
+        # Peripheral controller telemetry receiver
+        pkg-config
+        systemd
+      ];
+    };
+  };
+}
