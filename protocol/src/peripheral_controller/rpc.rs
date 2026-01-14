0358fc95333e85945390b45ecbf8bc3c76ac6877
Dan Nixon <dan@dan-nixon.com>
2026-01-14 18:11:53 +0000

    wip

--- protocol/src/peripheral_controller/rpc.rs
+++ protocol/src/peripheral_controller/rpc.rs
@@ -5,22 +5,47 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[cfg_attr(feature = "no-std", derive(defmt::Format))]
 pub enum Request {
-    Ping(u32),
     GetSystemInformation,
-
-    GetEventCount,
-    GetEventStatistics,
-    GetOldestEvent,
+    GetStringsMetadata,
+    GetString(usize),
+    GetTelemetryDataPoint,
 }
 
-#[allow(clippy::large_enum_variant)]
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[cfg_attr(feature = "no-std", derive(defmt::Format))]
 pub enum Response {
-    Ping(u32),
     GetSystemInformation(SystemInformation),
-
-    GetEventCount(usize),
-    GetEventStatistics(EventStatistics),
-    GetOldestEvent(Option<Event>),
+    GetStringsMetadata(StringsMetadata),
+    GetString(Option<StaticTelemetryString>),
+    GetTelemetryDataPoint(TelemetryDataPointResponse),
 }
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[cfg_attr(feature = "no-std", derive(defmt::Format))]
+pub struct StringsMetadata {
+    count: usize,
+    checksum: u8,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[cfg_attr(feature = "no-std", derive(defmt::Format))]
+pub struct TelemetryDataPointResponse {
+    data_point: Option<TelemetryDataPoint>,
+    more: bool,    
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[cfg_attr(feature = "no-std", derive(defmt::Format))]
+pub struct TelemetryDataPoint {
+    value: TelemetryValue,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[cfg_attr(feature = "no-std", derive(defmt::Format))]
+pub enum TelemetryValue {
+    Float32(f32),
+    String(TelemetryStringValue),
+}
+
+pub type StaticTelemetryString = heapless::String<64>;
+pub type TelemetryStringValue = heapless::String<32>;
