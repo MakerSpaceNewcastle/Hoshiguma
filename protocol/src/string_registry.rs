8f91719dd44e229a48253b7f21ca08f40c37fc42
Dan Nixon <dan@dan-nixon.com>
2026-01-17 17:38:59 +0000

    wip

--- protocol/src/string_registry.rs
+++ protocol/src/string_registry.rs
@@ -21,3 +21,35 @@ impl StringRegistry {
         self.strings.iter().position(|v| v == s)
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn basic_use() {
+        let mut sr = StringRegistry::default();
+
+        sr.push("feck".try_into().unwrap()).unwrap();
+        sr.push("arse".try_into().unwrap()).unwrap();
+        sr.push("drink".try_into().unwrap()).unwrap();
+
+        assert_eq!(sr.get_index("arse"), Some(1));
+
+        assert_eq!(sr.get_str(2), Some("drink"));
+    }
+
+    #[test]
+    fn get_index_not_found() {
+        let sr = StringRegistry::default();
+
+        assert!(sr.get_index("feck").is_none());
+    }
+
+    #[test]
+    fn get_str_not_found() {
+        let sr = StringRegistry::default();
+
+        assert!(sr.get_str(0).is_none());
+    }
+}
