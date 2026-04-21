03684e210994dab91ae2c3b6268138bd32931a87
Dan Nixon <dan@dan-nixon.com>
2026-04-21 12:13:14 +0100

    wip

--- firmware/lib/hoshiguma-common/src/telemetry.rs
+++ firmware/lib/hoshiguma-common/src/telemetry.rs
@@ -1,16 +1,16 @@
 use chrono::{DateTime, Utc};
 use core::fmt::{Display, Write};
 use heapless::String;
-use serde::{Deserialize, Serialize};
 
-pub fn format_influx_timestamp(timestamp: Option<DateTime<Utc>>) -> Result<String<20>> {
+pub fn format_influx_timestamp(
+    timestamp: Option<DateTime<Utc>>,
+) -> Result<String<20>, core::fmt::Error> {
     let mut s = String::new();
 
     if let Some(timestamp) = timestamp {
         let timestamp_ns = timestamp.timestamp_nanos_opt().unwrap();
 
-        s.write_fmt(format_args!(" {timestamp_ns}"))
-            .map_err(|_| Error::FormatError)?;
+        s.write_fmt(format_args!(" {timestamp_ns}"))?;
     }
 
     Ok(s)
@@ -21,14 +21,12 @@ pub fn format_influx_line<const LEN: usize, T: Display>(
     field: &str,
     value: T,
     timestamp: Option<DateTime<Utc>>,
-) -> Result<String<LEN>> {
+) -> Result<String<LEN>, core::fmt::Error> {
     let timestamp = format_influx_timestamp(timestamp)?;
 
     let mut line_str = String::new();
 
-    line_str
-        .write_fmt(format_args!("{measurement} {field}={value}{timestamp}"))
-        .map_err(|_| Error::FormatError)?;
+    line_str.write_fmt(format_args!("{measurement} {field}={value}{timestamp}"))?;
 
     Ok(line_str)
 }
@@ -38,23 +36,12 @@ pub fn format_influx_line_str<const LEN: usize, T: Display>(
     field: &str,
     value: T,
     timestamp: Option<DateTime<Utc>>,
-) -> Result<String<LEN>> {
+) -> Result<String<LEN>, core::fmt::Error> {
     let timestamp = format_influx_timestamp(timestamp)?;
 
     let mut line_str = String::new();
 
-    line_str
-        .write_fmt(format_args!("{measurement} {field}=\"{value}\"{timestamp}"))
-        .map_err(|_| Error::FormatError)?;
+    line_str.write_fmt(format_args!("{measurement} {field}=\"{value}\"{timestamp}"))?;
 
     Ok(line_str)
-}
-
-pub type Result<T> = core::result::Result<T, Error>;
-
-#[derive(Debug, defmt::Format, Clone, PartialEq, Serialize, Deserialize)]
-pub enum Error {
-    FormatError,
-    MissingString(&'static str),
-    NoStringAtIndex(usize),
 }
