06c86144a85d5ce80975affa6d57eee00dd79adb
Dan Nixon <dan@dan-nixon.com>
2026-04-26 09:13:52 +0100

    wip

--- firmware/lib/hoshiguma-common/src/network/cobs_framing.rs
+++ firmware/lib/hoshiguma-common/src/network/cobs_framing.rs
@@ -1,13 +1,11 @@
 use heapless::Vec;
 
 #[derive(Default)]
-pub struct CobsFramer {
-    buffer: Vec<u8, { Self::BUFFER_SIZE }>,
+pub struct CobsFramer<const BUFFER_SIZE: usize> {
+    buffer: Vec<u8, BUFFER_SIZE>,
 }
 
-impl CobsFramer {
-    pub const BUFFER_SIZE: usize = 4096;
-
+impl<const BUFFER_SIZE: usize> CobsFramer<BUFFER_SIZE> {
     pub fn len(&self) -> usize {
         self.buffer.len()
     }
@@ -16,7 +14,7 @@ impl CobsFramer {
         self.buffer.extend_from_slice(bytes).map_err(|_| ())
     }
 
-    pub fn next_message(&mut self) -> Option<Vec<u8, { Self::BUFFER_SIZE }>> {
+    pub fn next_message(&mut self) -> Option<Vec<u8, BUFFER_SIZE>> {
         // Find the next zero byte, which indicates the end of a COBS frame
         if let Some(pos) = self.buffer.iter().position(|&b| b == 0) {
             // Extract the frame up to and including the zero byte
@@ -34,22 +32,22 @@ mod test {
 
     #[test]
     fn initial_state() {
-        let mut framer = CobsFramer::default();
+        let mut framer = CobsFramer::<1024>::default();
         assert_eq!(framer.len(), 0);
         assert!(framer.next_message().is_none());
     }
 
     #[test]
     fn push_when_buffer_full() {
-        let mut framer = CobsFramer::default();
-        framer.push(&[1; CobsFramer::BUFFER_SIZE]).unwrap();
-        assert_eq!(framer.len(), CobsFramer::BUFFER_SIZE);
+        let mut framer = CobsFramer::<1024>::default();
+        framer.push(&[1; 1024]).unwrap();
+        assert_eq!(framer.len(), 1024);
         assert!(framer.push(&[1]).is_err());
     }
 
     #[test]
     fn simple() {
-        let mut framer = CobsFramer::default();
+        let mut framer = CobsFramer::<1024>::default();
 
         framer.push(&[1, 2, 3, 0]).unwrap();
         assert_eq!(framer.len(), 4);
@@ -61,7 +59,7 @@ mod test {
 
     #[test]
     fn single_frame_partial() {
-        let mut framer = CobsFramer::default();
+        let mut framer = CobsFramer::<1024>::default();
 
         framer.push(&[1, 2, 3]).unwrap();
         assert_eq!(framer.len(), 3);
@@ -77,7 +75,7 @@ mod test {
 
     #[test]
     fn multi_frame_partial() {
-        let mut framer = CobsFramer::default();
+        let mut framer = CobsFramer::<1024>::default();
 
         framer.push(&[1, 2, 3]).unwrap();
         assert_eq!(framer.len(), 3);
